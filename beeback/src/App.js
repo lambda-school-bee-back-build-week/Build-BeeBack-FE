676e8c3f5678596554c8514af06c4576349f8780
clem9281 <clem9281@gmail.com>
2019-04-17 12:58:59 -0700

    started adding maps

--- beeback/src/App.js
+++ beeback/src/App.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 import './App.css';
-import {Route, Link} from 'react-router-dom'
+import { Route, Link } from 'react-router-dom'
 import Loginpage from "./components/loginpage"
 import Registerpage from "./components/registerpage"
+import Main from "./components/main";
 
 
 class App extends Component {
@@ -10,16 +11,18 @@ class App extends Component {
     return (
       <div className="App">
         <div>
-        <nav>
-          <Link  to = "/"> Login</Link>
-          <Link to = '/registerpage'> Register </Link>
-        </nav>
+          <nav>
+            <Link to="/main">Main</Link>
+            <Link to="/"> Login</Link>
+            <Link to='/registerpage'> Register </Link>
+          </nav>
         </div>
 
-      <Route   exact path = "/" component = {Loginpage}/>
-      <Route path = "/registerpage" component = {Registerpage}/>
+        <Route exact path="/" component={Loginpage} />
+        <Route path="/registerpage" component={Registerpage} />
+        <Route path="/main" component={Main} />
 
-        
+
       </div>
     );
   }
