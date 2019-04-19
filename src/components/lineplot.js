1a744c7cd2b277f602e1283bc2f68779a3518909
Mark Oliver <mark.c.oliver@gmail.com>
2019-04-18 21:25:03 -0400

    us state name

--- src/components/lineplot.js
+++ src/components/lineplot.js
@@ -1,60 +1,50 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 // import {chartingData} from "../actions/index"
-import {connect} from 'react-redux'
+import { connect } from 'react-redux'
 import createPlotlyComponent from 'react-plotlyjs';
 // import Plotly from 'plotly.js-geo-dist';
 import Plotly from 'plotly.js/dist/plotly-cartesian';
+import './lineplot.css'
+import * as state_data from './us_states.json';
 const PlotlyComponent = createPlotlyComponent(Plotly);
 
 
+
 class lineplot extends Component {
-    
-    
-    
-    
-  render() {
-      console.log(this.props)
-      return (
-      <div>
-           
-            
 
-                
-                <PlotlyComponent
+
+    render() {
+        // console.log(this.props)
+        // console.log('state_data', state_data.default[this.props.us_state])
+        return (
+            <div>
+                <PlotlyComponent className='centered'
                     data={[
-                    {
-                    x: this.props.X,
-                    y: this.props.y,
-                    type: 'scatter',
-                    mode: 'lines+points',
-                    marker: {color: 'red'},
-                    },
-                    {type: 'bar', x:this.props.X, y:this.props.yb},
+                        {
+                            x: this.props.X,
+                            y: this.props.y,
+                            type: 'scatter',
+                            mode: 'lines+points',
+                            marker: { color: 'red' },
+                        },
+                        { type: 'bar', x: this.props.X, y: this.props.yb },
                     ]}
-                    layout={ {width: 320, height: 240, title: "k"} }
+                    layout={{ width: 640, height: 480, title: state_data.default[this.props.us_state].name}}
                 />
-                
-            
-            
-    
-            )}
             </div>
-            
-      
-
-      )
-  }
+        )
+    }
 }
 
 
 
-const mapStateToProps = state =>{
-    
-    return{
+const mapStateToProps = state => {
+
+    return {
         data: state.data
     }
 }
-export default connect (
+export default connect(
     mapStateToProps,
     // {chartingData}
 )(lineplot)
